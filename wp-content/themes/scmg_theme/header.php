e26a6a3915c79b0644f863a3508e8c74b68ebb6d
Tod Cribb <tdcribb@gmail.com>
2013-04-16 09:02:05 -0400

    added social share to articles

--- wp-content/themes/scmg_theme/header.php
+++ wp-content/themes/scmg_theme/header.php
@@ -35,6 +35,14 @@
 <link rel="stylesheet" type="text/css" media="all" href="/wp-content/css/scmg.css" />
 <script src="/wp-content/js/scmg.js"></script>
 
+<script type="text/javascript">
+  	(function() {
+   		var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+   		po.src = 'https://apis.google.com/js/plusone.js';
+   		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  	})();
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
--- wp-content/themes/scmg_theme/single.php
+++ wp-content/themes/scmg_theme/single.php
@@ -16,6 +16,23 @@ get_header(); ?>
 
 				<?php get_template_part( 'content', get_post_format() ); ?>
 
+				<div id="fb-root"></div>
+				<script>(function(d, s, id) {
+				  var js, fjs = d.getElementsByTagName(s)[0];
+				  if (d.getElementById(id)) return;
+				  js = d.createElement(s); js.id = id;
+				  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+				  fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));</script>
+				<div class="fb-like" data-send="true" data-width="350" data-show-faces="false"></div>
+
+				<div id="google-plus">
+					<div class="g-plusone" data-size="medium" data-annotation="inline" data-width="200"></div>
+				</div>
+				
+				<a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
 				<nav class="nav-single">
 					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
 					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
