e7c527f30edc1e70575dcfb076aee61f475dcfa8
Tod Cribb <tdcribb@gmail.com>
2013-04-30 11:47:15 -0400

    add submit artist page

---
+++ wp-content/themes/scmg_theme/submit-artist.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: Submit Artist
+*/
+?>
+<?php get_header(); ?>
+
+	<?php echo do_shortcode('[gravityform id=3 title=false description=false ajax=true]'); ?>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
