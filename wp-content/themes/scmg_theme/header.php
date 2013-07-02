c5fcf7dbded01f0da175c32d10a1d9cff14e1262
Tod Cribb <tdcribb@gmail.com>
2013-07-02 10:26:41 -0400

    update to header

--- wp-content/themes/scmg_theme/header.php
+++ wp-content/themes/scmg_theme/header.php
@@ -67,7 +67,7 @@
 		<div id="header-title">
 			<a href="/">
 				South Carolina</br>
-				Music Guide
+				<span class="music-red">Music</span> Guide
 			</a>
 		</div>
 		<div id="header-top-links">
@@ -95,7 +95,7 @@
 			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
 			
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-			<div id="header-nav-divider"></div>
+			<!-- <div id="header-nav-divider"></div> -->
 		</nav><!-- #site-navigation -->
 		<?php $header_image = get_header_image();
 		if ( ! empty( $header_image ) ) : ?>
